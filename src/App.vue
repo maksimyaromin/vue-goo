988990a6c9e89be3c38addfbc728fbfdeb513760
Maksim Yaromin <Maksim_Yaromin@epam.com>
2019-12-05 17:16:36 +0300

    Vue GOO

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="boxes boxes--source">
+            <Box
+                v-for="name in names"
+                :key="name"
+                :name="name"
+                :isDraggable="true"
+                :onDragStart="onDragStart"
+            />
+        </div>
+
+        <button v-on:click="onClick" class="reset-button">Reset</button>
+
+        <div
+            class="boxes boxes--destination"
+            v-on:dragover="onDragOver"
+            v-on:drop="onDrop"
+        >
+            <Box
+                v-for="name in acceptedNames"
+                :key="name"
+                :name="name"
+                :isAccepted="true"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Box from "./components/Box.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    const namesDB = [
+        'Maksim',
+        'Good Kat',
+        'Patrick O\'Brian'
+    ];
+
+    export default {
+        name: "app",
+        components: {
+            Box
+        },
+        data: function () {
+            return {
+                names: [ ...namesDB ],
+                acceptedNames: []
+            }
+        },
+        methods: {
+            onDragStart: function (event) {
+                if (event.target) {
+                    event.dataTransfer.setData(
+                        'text/plain',
+                        event.target.attributes.getNamedItem('data-name').value
+                    );
+                }
+            },
+            onDragOver: function (event) {
+                event.preventDefault();
+            },
+            onDrop: function (event) {
+                const name = event.dataTransfer.getData('text');
+
+                this.names = this.names.filter(na => na !== name);
+                this.acceptedNames = [
+                    ...this.acceptedNames,
+                    name
+                ];
+
+                event.dataTransfer.clearData();
+            },
+            onClick: function () {
+                this.names = [ ...namesDB ];
+                this.acceptedNames = [];
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .container {
+        display: flex;
+        justify-content: space-between;
+        margin: 20px auto;
+        max-width: 798px;
+    }
+
+    .boxes {
+        background-color: #E6E6E6;
+        box-sizing: border-box;
+        padding: 10px;
+        width: 100px;
+    }
+
+    .reset-button {
+        background: transparent;
+        border: 0;
+        border-radius: 0;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+        cursor: pointer;
+        font-weight: bold;
+        outline: none!important;
+        text-transform: uppercase;
+    }
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="box" :data-name="name" :class="{ 'box--green': isAccepted }" :draggable="isDraggable" v-on:dragstart="onDragStart">
+        <strong>{{name}}</strong>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Box",
+        props: {
+            name: String,
+            isDraggable: {
+                type: Boolean,
+                default: false
+            },
+            isAccepted: {
+                type: Boolean,
+                default: false
+            },
+            onDragStart: {
+                type: Function,
+                default: function () {}
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .box {
+        align-items: center;
+        background-color: blue;
+        display: flex;
+        justify-content: center;
+        height: 80px;
+        width: 80px;
+    }
+
+    .box.box--green {
+        background-color: green;
+    }
+
+    .box {
+        cursor: pointer;
+    }
+
+    .box:not(:last-child) {
+        margin-bottom: 10px;
+    }
+
+    strong {
+        color: white;
+        text-align: center;
+    }
+</style>
